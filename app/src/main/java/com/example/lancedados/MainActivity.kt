4de4bea322b8059f703baee0c6fea83b7173a82f
Juliana Cuani <juliana.271@hotmail.com>
2020-10-24 09:26:57 -0300

    App funcionando

--- app/src/main/java/com/example/lancedados/MainActivity.kt
+++ app/src/main/java/com/example/lancedados/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.lancedados
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
@@ -10,13 +11,31 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val botaoLancarDados = findViewById<Button>(R.id.lanceDadosBotao)
+        var botaoLancarDados = findViewById<Button>(R.id.lanceDadosBotao)
+        var dado1 = findViewById<ImageView>(R.id.dado1)
+        var dado2 = findViewById<ImageView>(R.id.dado2)
+
 
          botaoLancarDados.setOnClickListener {
-
+          dado1.setImageResource(lançaDado())
+          dado2.setImageResource(lançaDado())
       }
    }
-    fun lancarDados(): Int {
+
+    fun lançaDado(): Int {
+        return recuperaFaceDado(geraNumero())
+    }
+      fun recuperaFaceDado(valor:Int): Int{
+        return  when (valor){
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+    }
+    fun geraNumero(): Int {
         return (1..6).random()
     }
 }
